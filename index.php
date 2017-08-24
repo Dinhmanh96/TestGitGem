5c62affa4c59c387f77666019606eba6ad052555
Dinhmanh96 <Dinhmanh96@gmail.com>
2017-08-24 14:04:00 +0700

    Creat index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		
+	</header>
+	
+	<body>
+		
+	</body>
+
+	<footer>
+		
+	</footer>
+</body>
+</html>
